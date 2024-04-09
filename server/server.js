c3967826f6cd8269564bcbaab683d39cf07b850a
Kyle Tseng <kyletseng1699@gmail.com>
2024-04-09 15:38:51 -0700

    sameSite: none

--- server/server.js
+++ server/server.js
@@ -1,39 +1,39 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const cors = require("cors");
-const session = require("express-session");
-const http = require("http");
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const session = require('express-session');
+const http = require('http');
 const WebSocket = require('ws');
-const startWebSocketServer = require("./webSocket/WebsocketServer");
-require("dotenv").config();
+const startWebSocketServer = require('./webSocket/WebsocketServer');
+require('dotenv').config();
 
 // Routes import
-const { userRoutes } = require("./api/User/User.routes");
-const { lobbyRoutes } = require("./api/Lobby/Lobby.routes");
-const { authRoutes } = require("./auth/routes");
-const { gameRoutes } = require("./gameApi/routes");
+const { userRoutes } = require('./api/User/User.routes');
+const { lobbyRoutes } = require('./api/Lobby/Lobby.routes');
+const { authRoutes } = require('./auth/routes');
+const { gameRoutes } = require('./gameApi/routes');
 
 // Store
-const { store } = require("./auth/constants");
+const { store } = require('./auth/constants');
 
 const app = express();
 
 const PORT = process.env.PORT;
 
 const sessionParser = session({
-  name: "session",
+  name: 'session',
   secret: process.env.SESSION_SECRET,
   resave: false,
   saveUninitialized: false,
-  cookie: { httpOnly: false, sameSite: true, maxAge: 86400000, unset: "destroy" }, // not sure if should set to destroy
+  cookie: { httpOnly: false, sameSite: 'none', maxAge: 86400000, unset: 'destroy' }, // not sure if should set to destroy
   store,
-})
+});
 
 app.use(
   cors({
     origin: process.env.FRONTEND_URL,
     credentials: true,
-  }),
+  })
 );
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -62,10 +62,10 @@ mongoose
     // start websocket server
     startWebSocketServer(sessionParser, server);
     // Routes
-    app.use("/api/user", userRoutes);
-    app.use("/api/lobby", lobbyRoutes);
-    app.use("/api/auth", authRoutes);
-    app.use("/api/game", gameRoutes);
+    app.use('/api/user', userRoutes);
+    app.use('/api/lobby', lobbyRoutes);
+    app.use('/api/auth', authRoutes);
+    app.use('/api/game', gameRoutes);
 
     server.listen(PORT, () => {
       console.log(`Server is running on port ${PORT}`);
